c429a5d71478ee1e64a77c05ecdceb0c9f3a5cac
Plant900 <rythodir@gmail.com>
2022-11-23 18:22:24 -0700

    asdf

--- backend/server.js
+++ backend/server.js
@@ -21,18 +21,18 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/messages', router)
 
-//serve frontend
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/dist')))
-//   app.get('*', (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
-//     )
-//   )
-// } else if (process.env.NODE_ENV === 'development') {
-//   app.get('/', (req, res) => res.send('Please set to production'))
-// } else {
-//   app.get('/', (req, res) => res.send('what are you doing'))
-// }
+// serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
+    )
+  )
+} else if (process.env.NODE_ENV === 'development') {
+  app.get('/', (req, res) => res.send('Please set to production'))
+} else {
+  app.get('/', (req, res) => res.send('what are you doing'))
+}
 
 app.listen(port, () => console.log(`server started at ${port}`))
