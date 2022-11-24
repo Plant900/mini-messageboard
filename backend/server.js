5769d04f2970aae244a08ae998920b145dc56736
Plant900 <rythodir@gmail.com>
2022-11-23 18:05:57 -0700

    message

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
-const port = process.env.PORT || 5000
+const port = process.env.PORT
 const router = require('./routes/messageRoutes')
 const connectDB = require('./config/db')
 const cors = require('cors')
